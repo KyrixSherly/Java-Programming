7461601e3baa8c05001159020b4dfb921eed7572
KyrixSherly <euyjae123@Naver.com>
2017-04-03 13:16:02 +0900

    Add files via upload

--- src/com/sd/gui/CalcAreaListener.java
+++ src/com/sd/gui/CalcAreaListener.java
@@ -1,11 +1,14 @@
 package com.sd.gui;
 
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class CalcAreaListener implements ActionListener {
   public void actionPerformed(ActionEvent e){
     System.out.println("Hello");
-    JButton b=(JButtom)e.getSource();
+    JButton b=(JButton)e.getSource();
     String cmd=e.getActionCommand();
     if(cmd.equals("계산")) {
       System.out.println("계산 버튼");
@@ -13,7 +16,7 @@ public class CalcAreaListener implements ActionListener {
     }
     else if(cmd.equals("취소")) {
       System.out.println("취소 버튼");
-      b.setText("취소했습니다");
+      b.setText("취소되었습니다.");
     }
   }
 }
