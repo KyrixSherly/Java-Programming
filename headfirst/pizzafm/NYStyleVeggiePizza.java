c0aab0a8860930c8cdcda81a016b58a3da6de89b
KyrixSherly <euyjae123@Naver.com>
2017-04-18 22:09:05 +0900

    Add files via upload

---
+++ headfirst/pizzafm/NYStyleVeggiePizza.java
@@ -0,0 +1,10 @@
+package headfirst.factory.pizzafm;
+
+public class NYStyleVeggiePizza extends Pizza {
+  public NYStyleVeggiePizza(){
+    name="NYStyleVeggie Pizza";
+    dough = "Thin Crust";
+    sauce = "Marinara Pizza Sauce";
+    toppings.add("Grated Reggiano Cheese");
+  }
+}
