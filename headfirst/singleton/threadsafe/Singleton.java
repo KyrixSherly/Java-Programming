58739a368ed0517bd4d36d023fced1730d1cc6f7
KyrixSherly <euyjae123@Naver.com>
2017-05-15 13:48:10 +0900

    Add files via upload

--- headfirst/singleton/threadsafe/Singleton.java
+++ headfirst/singleton/threadsafe/Singleton.java
@@ -1,8 +1,7 @@
 package headfirst.singleton.threadsafe;
 
 public class Singleton{
-  //      공유의 의미가 있음.
-  private volatile static Singleton uniqueInstance;
+  private static Singleton uniqueInstance;
   private static int numCalled=0;
   
   private Singleton() {
